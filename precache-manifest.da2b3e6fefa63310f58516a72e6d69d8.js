26792c61791c6fd54681a8eae5c4afeef6f3a0df
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2019-12-14 18:34:30 -0400

    Updates

---
+++ precache-manifest.da2b3e6fefa63310f58516a72e6d69d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc356c708b7007621125377646e89cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06a56b1a53d4941f36e5",
+    "url": "/static/css/main.08a8b70f.chunk.css"
+  },
+  {
+    "revision": "a1f9cadb10426753bb90",
+    "url": "/static/js/2.429e7af1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.429e7af1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06a56b1a53d4941f36e5",
+    "url": "/static/js/main.e50cf908.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
